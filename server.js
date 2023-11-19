7113cd63c97c0a322aeb182373ed84be063a06a3
dora.medvaric <dora.medvaric@xaidat.com>
2023-11-19 17:54:53 +0100

    server files

--- server.js
+++ server.js
@@ -19,6 +19,25 @@ const fs = require('fs').promises;
 
 // Serve static files (build folder) for the React app
 //app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.post('/api/emailVerification', jsonParser,async (req, res) => {
   const userPsiholog = req.body;
 
